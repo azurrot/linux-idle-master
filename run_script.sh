fe0bb4424f7232c01f2ca915c054d84a7b422fde
azurrot <marco.schleyer@gmail.com>
2025-06-02 10:07:03 +0200

    Create run_script.sh

---
+++ run_script.sh
@@ -0,0 +1,14 @@
+# Pfade
+VENV_PATH="/path/to/myenv"
+SCRIPT_PATH="/path/to/linux-steam-idle/start.py"
+
+# Alte Instanz beenden (falls läuft)
+PID=$(pgrep -f "$SCRIPT_PATH")
+if [ -n "$PID" ]; then
+    echo "Killing old instance with PID $PID"
+    kill "$PID"
+fi
+
+# Aktivieren des virtuellen Environments und starten des Scripts
+source "$VENV_PATH/bin/activate"
+python3 "$SCRIPT_PATH"
