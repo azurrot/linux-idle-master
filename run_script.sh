be0e9696edd081ee5b019de38fa190ce483799c6
azurrot <marco.schleyer@gmail.com>
2025-06-02 10:09:26 +0200

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -1,3 +1,8 @@
+# This script stops any running instance of the target Python script,
+# then activates the specified virtual environment and starts the script.
+# It's used to ensure only one instance of the application runs at a time.
+
+
 # Pfade
 VENV_PATH="/path/to/myenv"
 SCRIPT_PATH="/path/to/linux-steam-idle/start.py"
