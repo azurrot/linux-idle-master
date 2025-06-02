18018a2a071065bdd0b9bfd69a67cd630e5716f3
azurrot <marco.schleyer@gmail.com>
2025-06-02 10:06:29 +0200

    Update run_cookies.sh

--- run_cookies.sh
+++ run_cookies.sh
@@ -1,1 +1,16 @@
-test
+
+
+# This script activates a Python virtual environment and sets required GUI environment variables
+# to simulate a graphical desktop session (as used in PyCharm).
+# It then runs the Python script that updates Steam cookies using Firefox and Selenium.
+# Venv aktivieren
+source /path/to/.venv/bin/activate
+
+# GUI-Umgebungsvariablen wie in PyCharm setzen
+# export DISPLAY=:0
+# export XAUTHORITY=/run/user/1000/.mutter-Xwaylandauth.6HWV62
+# export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+# export WAYLAND_DISPLAY=wayland-0
+
+# Script ausführen
+python3 path/to/update_cookies.py
